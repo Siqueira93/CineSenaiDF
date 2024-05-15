a877a73281fe22c436259f5d73bff1bf81edbe1d
RaulSSiqueira <raul.soaressiqueira@gmail.com>
2024-05-15 16:42:04 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function filmeUm() {
+    let info = document.querySelector("#informacoes")
+    info.style.display = 
+    document.getElementById('exemplo').style.display = 'block';
+}
+function fecharfilmeUm() {
+    document.getElementById('exemplo').style.display = 'none';
+}
+
+
+function filmeDOIS() {
+    document.getElementById('informacoes').style.display = 'block';
+}
+function fecharfilmeDOIS() {
+    document.getElementById('informacoes').style.display = 'none';
+}
+
+function filmeTres() {
+    document.getElementById('informacoes').style.display = 'block';
+}
+function fecharfilmeTres() {
+    document.getElementById('informacoes').style.display = 'none';
+}
